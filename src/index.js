ff5b25f2af5089768489045c441655a9e1355fbf
Tomato6966 <chris.pre03@gmail.com>
2023-05-06 13:40:10 +0200

    Tutorial-#2

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { config } from "dotenv";
-import { yourBotClient } from "./structure/Client.js";
-config();
-
-const client = new yourBotClient();
-
-client.on("finishedLoadingBot", () => {
-    console.log("BOT IS FINISHED LOADING and now LOGS IN TO DISCORD");
-})
